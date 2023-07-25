5c82746ff278265e81de1c051405465e7fe1c433
deep-poharkar <deeppoharkar21@gmail.com>
2023-07-25 18:09:53 +0530

    added sliding window 01

---
+++ .history/Sliding Window/01 Maximum Sum Subarray of size K_20230725180846.cpp
@@ -0,0 +1,24 @@
+// Identification of the problem: arr or str given , subarray or substring asked , window size or condition given, max or min asked.
+//  Sliding Window Technique: Fixed Size Window
+int maximumSumSubarray(int k, vector<int> &Arr, int n)
+{
+    int i, j = 0;
+    int sum = 0;
+    int maxSum = INT_MIN;
+    while (j < n)
+    {
+        sum = sum + Arr[j]; // calculation of sum to reduce time complexity
+        if (j - i + 1 < k)
+        {
+            j++; // inc j till window size is reached
+        }
+        else if (j - i + 1 == k)
+        {                              // when window size is reached
+            maxSum = max(maxSum, sum); // update maxSum
+            sum = sum - Arr[i];        // remove the first element of the window
+            i++;
+            j++;
+        }
+    }
+    return maxSum;
+};
